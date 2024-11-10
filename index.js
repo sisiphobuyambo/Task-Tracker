5e9747231f9fdba5fd9177a1d5905cf281f14d6d
MissSunflower25 <153073194+MissSunflower25@users.noreply.github.com>
2024-11-10 08:58:54 +0200

    .

--- index.js
+++ index.js
@@ -2,75 +2,99 @@ const taskTrackerContainer = document.getElementById("task-tracker-outer-contain
 const taskBoard = document.getElementById("task-board");
 const addBtn = document.getElementById("add-task-btn");
 const inputField = document.getElementById("task-input-field");
-let innerText;
 let isChecked = false;
 let isEdit = false;
 
 const itemsArray =[]
+let nextTaskId = 0;
+let labelId = 0;
 
-// FUNCTIONS
-const addListItem =(el)=>{  
-                            taskBoard.innerHTML += el
-                            itemsArray.push(el)
-                            inputField.value = ""
-                            console.log(itemsArray)
-                        }
-
-const checkItem = (el)=>{ if(!isChecked){
-                            el.classList.add("line-through");
-                            // itemsArray.push(itemsArray.splice(itemsArray.indexOf(), 1)[0])
-                            // console.log(itemsArray.indexOf(el.parentElement.parentElement))
-                            isChecked = !isChecked;
-                         }else{
-                            el.classList.remove("line-through");
-                            isChecked = !isChecked;}
-                        }
-
-const deleteItem =(el)=>{  el.parentElement.parentElement.parentElement.remove()
-                        }
 
 // APPEND TASK TO DOM
 
 taskTrackerContainer.addEventListener("click", (e)=>{
 
     const event = e.target;
-
-    const taskItemHtml = `<div class="task-list-container">
-                            <div class="task-item-styling">
-                                <label id="check" class="task-item-label">
-                                    <input type="checkbox"> ${inputField.value}
-                                    <span class="checkbox-container"></span>
-                                </label>
-                                <div class="task-icons">
-                                    <i id="edit" class="fa-solid fa-pencil"></i>
-                                    <i id="delete" class="fa-solid fa-trash"></i>
-                                </div>
-                            </div>
-                            <hr>
-                        </div>`
-
+    let taskDescription = inputField.value;
+          
 
     if(event.id === "add-task-btn"){
-       addListItem(taskItemHtml)
+       addListItem()
 
     }else if(event.id ==="check"){
-        checkItem(event)
+        checkItem()
 
     }else if(event.id === "edit"){
-        // innerText = event.parentElement.parentElement.children[0].innerText;
-        
-        
-        
         isEdit = true;
-
 
     }else if(event.id === "delete"){
         deleteItem(event)
     }
     
+
+
+// FUNCTIONS
+
+    function addListItem(){
+
+        if(inputField.value === ""){
+            event.disable
+
+        }else{
+
+            const taskItemHtml = `<div class="task-list-container">
+                                    <div class="task-item-styling">
+                                        <label data-id="${++labelId}" id="check" class="task-item-label">
+                                            <input type="checkbox"> ${inputField.value}
+                                            <span class="checkbox-container"></span>
+                                        </label>
+                                        <div class="task-icons">
+                                            <i id="edit" class="fa-solid fa-pencil"></i>
+                                            <i id="delete" class="fa-solid fa-trash"></i>
+                                        </div>
+                                    </div>
+                                    <hr>
+                                </div>`
+
+            taskBoard.innerHTML += taskItemHtml
+            inputField.value = ""
+
+            const listItemDetails = {
+                id: ++nextTaskId,
+                description: taskDescription,
+                isComplete: false
+            }
+
+            itemsArray.push(listItemDetails)
+        }
+    }
+
+    function checkItem(){
+
+        const itemDetails = itemsArray.find(obj => obj.id === parseInt(event.dataset.id))
+        let {isComplete} = itemDetails
+
+        // console.log(typeof itemsArray[0].id)
+        // console.log(typeof parseInt(event.dataset.id))
+        // console.log(isComplete)
+
+        if(!isComplete){
+            event.classList.add("line-through");
+        }else{
+            event.classList.remove("line-through");
+        }
+
+        {isComplete = !isComplete}
+        // isComplete = !isComplete
+        console.log(itemsArray)
+
+    }
+
+    function deleteItem(el){
+        el.parentElement.parentElement.parentElement.remove()
+    }
+
 }
 )
 
 
-
-// STRIKETHROUGH TASK ON CHECK
